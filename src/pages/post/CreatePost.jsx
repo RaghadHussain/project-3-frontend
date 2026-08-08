4d02dfcbf20bee64b13f8280ecf31af8803d0a61
Your Name <you@example.com>
2026-08-08 19:21:54 +0300

    create Post Page

--- src/pages/post/CreatePost.jsx
+++ src/pages/post/CreatePost.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import { useState } from 'react'
+import { createPost } from '../../services/postService'
+import { useNavigate } from 'react-router'
 
 function CreatePost() {
+
+  const [formData, setFormData] = useState({
+    title: '',
+    caption: '',
+    image: '',
+    category: ''
+  })
+
+  const navigate = useNavigate()
+
+  function handleChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  function handleImageChange(event) {
+    setFormData({ ...formData, image: event.target.files[0] })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const createdPost = await createPost(formData)
+    navigate(`/post/${createdPost._id}`)
+  }
+
   return (
-    <div>CreatePost</div>
+    <>
+      <h1 className='title'>Add New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title: </label>
+        <input type="text" name="title" id="title" value={formData.title} onChange={handleChange} />
+
+        <label htmlFor="caption">Caption: </label>
+        <textarea name="caption" id="caption" value={formData.caption} onChange={handleChange} />
+
+        <label htmlFor="image">Upload Image: </label>
+        <input id="image" name="image" type="file" accept="image/*" onChange={handleImageChange} />
+
+        <label htmlFor="category">Category: </label>
+        <select required name="category" id="category" value={formData.category} onChange={handleChange}>
+          <option value="">Select A Category</option>
+          <option value="fashion">Fashion</option>
+          <option value="skincare">Skin Care</option>
+          <option value="lifestyle">Lifestyle</option>
+          <option value="hobbies">Hobby</option>
+        </select>
+        <button type="submit">Create Post</button>
+      </form>
+    </>
   )
 }
 
