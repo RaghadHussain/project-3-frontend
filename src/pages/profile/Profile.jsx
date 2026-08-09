39e791e29f3fdaabf26696233d96498e0f6feff7
Your Name <you@example.com>
2026-08-09 16:17:41 +0300

    Profile Page

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router'
+import { getUserById, followUser, unfollowUser } from '../../services/authService'
+import { useAuth } from '../../context/AuthContext'
+import { getUserPosts } from '../../services/postService'
+
+function Profile() {
+  const { user } = useAuth()
+  const { id } = useParams()
+
+  const [info, setInfo] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [post, setPost] = useState([])
+
+
+
+  useEffect(() => {
+    async function loadInfo() {
+      try {
+        const userInfo = await getUserById(id)
+        const userPosts = await getUserPosts(id)
+
+        setInfo(userInfo)
+        setPost(userPosts)
+      } catch (e) {
+        console.log(e)
+        setError('Failed to Load Profile Information!')
+      } finally {
+        setLoading(false)
+      }
+    }
+    loadInfo()
+  }, [id])
+
+  async function handleFollow() {
+    try {
+      const isFollowing = info.followers.some((oneId) => oneId === user._id)
+
+      const updatedUser = isFollowing
+        ? await unfollowUser(id)
+        : await followUser(id)
+
+      setInfo(updatedUser)
+    } catch (e) {
+      console.log(e)
+      setError('Failed to Follow/ Unfollow User!')
+    }
+  }
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}</p>
+
+  const userProfile = user && user._id === info._id
+  const isFollowing = user && info.followers.some((oneId) => oneId === user._id)
+
+  return (
+    <div>
+      <h4>{info.username}</h4>
+      {info.profileImage && <img src={info.profileImage} alt={info.username} width="200" />}
+      <p>{info.bio}</p>
+      <div>
+        <span>{info.followers.length} Followers</span>{" "}
+        <span>{info.followings.length} Following</span>
+      </div>
+      {!userProfile && user && (
+        <button onClick={handleFollow}>
+          {isFollowing ? "Unfollow" : "Follow"}
+        </button>
+      )}
+
+      <div>
+      {post.map(onePost => (
+        <div key={onePost._id}>
+          <h4>{onePost.user.username}</h4>
+          {onePost.category && <p>{onePost.category}</p>}
+          <h2>{onePost.title}</h2>
+          {onePost.image && <img src={onePost.image} alt={onePost.title} width="200" />}
+          <p>{onePost.caption}</p>
+          <p>Created At: {new Date(onePost.createdAt).getFullYear()}/{new Date(onePost.createdAt).getMonth() }/{new Date(onePost.createdAt).getDate()}</p>
+          <Link to={`/post/${onePost._id}`}>+</Link>
+        </div>
+      ))}
+      </div>
+    </div>
+  )
+}
+
+export default Profile
