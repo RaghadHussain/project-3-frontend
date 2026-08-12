b3956115d4f9c33094d7bbae61cbc3053e0659a3
zainabaliammarali-cloud <zainabaliammarali@gmail.com>
2026-08-12 15:01:31 +0300

    css

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,61 +1,61 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router'
-import { search } from '../services/authService'
-
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router";
+import { search } from "../services/authService";
 
 function Search() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [query, setQuery] = useState('')
-    const [results, setResults] = useState([])
-    const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true);
+    async function searchResults() {
+      try {
+        const response = await search(query);
+        setResults(response);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    searchResults();
+  }, [query]);
+  return (
+    <div>
+      <input
+        type="text"
+        id="searchBox"
+        placeholder="🔍︎ Search User Name Here ..."
+        value={query}
+        onChange={(event) => {
+          setQuery(event.target.value);
+        }}
+      />
 
-
-    useEffect(() => {
-        setLoading(true)
-        async function searchResults() {
-            try {
-                const response = await search(query)
-                setResults(response)
-            } catch (e) {
-                console.log(e)
-            } finally {
-                setLoading(false)
-            }
-
-
-
-        }
-                    searchResults()
-
-    }, [query])
-    return (
-        <div>
-            <input type="text" 
-            id='searchBox'
-             placeholder='🔍︎ Search User Name Here ...'
-              value={query}
-              onChange={(event)=>{setQuery(event.target.value)}}/>
-
-              <div>
-                {loading && 'Loading...'}
-                {results.length > 0 && (
-                    <ul>
-                        {results.map(user => 
-                            <li key={user._id}>
-                                <Link to={`/${user._id}`}><strong>{user.username}</strong></Link>
-                            </li>
-                        )}
-                    </ul>
-                )}
-
-                {query && results.length === 0 && !loading && (
-                    <strong>No Matching Results ...</strong>
-                )}
+      <div>
+        {loading && "Loading..."}
+        {results.length > 0 && (
+          <div>
+            {results.map((user) => (
+              <div key={user._id}>
+                <Link to={`/${user._id}`}>
+                  <strong>{user.username}</strong>
+                  <hr />
+                </Link>
               </div>
-        </div>
-    )
+            ))}
+          </div>
+        )}
+
+        {query && results.length === 0 && !loading && (
+          <strong>No Matching Results ...</strong>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
